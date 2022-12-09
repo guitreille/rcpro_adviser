1581fc49f11e70f95237def5cbcc73ba13449cc1
guitreille <guillaume.treille.de.grandsaigne@vub.be>
2022-12-09 11:36:42 +0100

    Added API code

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,13 @@
+
+
 class HomeController < ApplicationController
   def index
+    base_uri = 'https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY'
+    response = HTTParty.get(base_uri)
+    @response = JSON.parse(response.body) if response.code == 200
   end
 
   def about 
   end 
+
 end
